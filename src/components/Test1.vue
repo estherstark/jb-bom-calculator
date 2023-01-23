d882252c482e55c98e1b939d455da3c9279ac396
esther <phattara.nrrt@gmail.com>
2023-01-23 10:26:29 +0000

    Update 2 files

--- src/components/Test1.vue
+++ src/components/Test1.vue
@@ -1,10 +1,10 @@
+/* eslint-disable */
 <template>
   <div>
-    <v-toolbar color="grey darken-3" dense dark>
-      <v-img
-        :src="require('https://www.jenbunjerd.com/images/logo-menu.png')"
-      />
-      <v-appbar-nav-icon> </v-appbar-nav-icon>
+    <v-toolbar color="grey darken-4" dense dark>
+      <v-appbar-nav-icon>
+        <v-img :src="require('../assets/logo-jb.png')" />
+      </v-appbar-nav-icon>
       <v-toolbar-title>JB BoM Calculator</v-toolbar-title>
     </v-toolbar>
   </div>
