678b5a86c1f3e5d3fadb6e20341f6edf0a641362
esther <phattara.nrrt@gmail.com>
2023-01-24 10:28:36 +0000

    Update App.vue and Test1.vue

--- src/components/Test1.vue
+++ src/components/Test1.vue
@@ -1,62 +1,51 @@
 /* eslint-disable */
 <template>
   <div>
-    <v-toolbar color="grey darken-4" dense dark>
+    <v-toolbar color="grey darken-4" elevate-on-scroll>
       <a href="#default" class="logo">
         <img
           src="https://www.jenstore.com/media/wysiwyg/home/jbj-logo-footer.png"
           alt="JB Logo"
-          style="height:30px;"
+          style="height:30px;
+          "
         />
       </a>
-      <v-spacer></v-spacer>
+      <v-spacer />
       <v-toolbar-title class="white--text">JB BoM Calculator</v-toolbar-title>
-      <v-spacer></v-spacer>
+      <v-spacer />
       <v-btn class="black--text" depressed color="yellow" accent-4>login</v-btn>
     </v-toolbar>
+    <v-card class="pa-5 mx-lg-auto" outlined tile>
+      <h2 class="font-weight-regular">Welcome to <br /></h2>
+      <h1>JB BoM Calculator</h1>
+      <img
+        src="../assets/img-login.png"
+        responsive
+        style="max-width: 50%;
+          max-height: 50%;"
+      />
+    </v-card>
 
-    <v-container
-      v-for="align in alignments"
-      :key="align"
-      class="grey lighten-5 mb-6"
-      ><v-row :align="align" no-gutters>
-        <v-col>
-          <v-card class="pa-8" outlined tile>
-            <h2>Welcome to <br /></h2>
-            <h1>JB BoM Calculator</h1>
-            <img src="../assets/img-login.png" style="height:400px;" />
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-container>
+      <v-card class="pa-3 rounded-lg" color="grey lighten-3" width="400px">
+        <h2 class="text-center">Please Log In</h2>
 
-    <v-container fluid>
-      <v-form>
-        <v-text-field solo label="Append"></v-text-field>
-
-        <v-card
-          class="pa-md-4 mx-lg-auto"
-          color="grey lighten-2"
-          width="500px"
-          elevation="1"
-          text-center
-          secondary
-          text-no-wrap
-          rounded-lg
-        >
-          <h2 class="text-subtitle-2 text-center">Please Log In</h2>
-
-          <v-text-field
-            label="Employee ID"
-            outlined
-            :rules="rules"
-          ></v-text-field>
-          <v-text-field label="Password" outlined :rules="rules"></v-text-field>
-          <v-btn class="login-btn" depressed color="yellow" accent-4>
-            login
-          </v-btn>
-        </v-card>
-      </v-form>
+        <v-text-field
+          label="Employee ID"
+          color="white"
+          outlined
+          :rules="rules"
+        ></v-text-field>
+        <v-text-field
+          label="Password"
+          color="white"
+          outlined
+          :rules="rules"
+        ></v-text-field>
+        <v-btn class="login-btn" depressed color="yellow" accent-4>
+          login
+        </v-btn>
+      </v-card>
     </v-container>
   </div>
 </template>
@@ -65,7 +54,7 @@
 export default {
   name: "Test1",
   data: () => ({
-    alignments: ["start"]
+    alignments: ["start,end"]
   })
 };
 </script>
