73c99b1fce14bd7fb82fb10e5d864ec30c1a0b23
esther <phattara.nrrt@gmail.com>
2023-01-25 08:20:04 +0000

    Update 9 files (LoginView almost complete)

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,19 @@
 <template>
   <v-app>
     <div id="app">
-      <Test1Vue />
+      <LoginView />
     </div>
-
-    <v-app-bar app>
-      <!-- -->
-    </v-app-bar>
-
-    <!-- Sizes your content based upon application components -->
-    <v-main>
-      <!-- Provides the application the proper gutter -->
-      <v-container fluid>
-        <!-- If using vue-router -->
-        <router-view></router-view>
-      </v-container>
-    </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
 // eslint-disable-next-line
-import TestComponent from "./components/TestComponent.vue";
-// eslint-disable-next-line
-import Test1Vue from "./components/Test1.vue";
+import LoginView from "./components/LoginView.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
-    TestComponent,
-    Test1Vue
+    LoginView
   }
 };
 </script>
---
+++ src/components/LoginView.vue
@@ -0,0 +1,82 @@
+/* eslint-disable */
+<template>
+  <div>
+    <v-app-bar>
+      <v-toolbar color="grey darken-4" elevate-on-scroll>
+        <a href="#default" class="logo">
+          <img
+            src="../assets/jb-logo-mini.png"
+            alt="JB Logo"
+            style="height:34px;"
+            class="pa1"
+          />
+        </a>
+        <v-spacer />
+        <v-toolbar-title class="white--text hidden-sm-and-down"
+          >JB BoM Calculator</v-toolbar-title
+        >
+        <v-spacer />
+        <v-btn class="black--text" depressed color="yellow" accent-4>
+          please login</v-btn
+        >
+      </v-toolbar>
+    </v-app-bar>
+
+    <v-container
+      class="d-flex align-content-center flex-wrap"
+      grid-list-lg
+      responsive
+    >
+      <v-row justify="center" class="flex-wrap">
+        <v-card class="pa-4 ma-2 d-flex justify-center align-center">
+          <v-col class="order-2" style="min-width: 150px;" cols="12" md="1">
+            <h2 class="font-weight-regular">Welcome to<br /></h2>
+            <h1>JB BoM Calculator</h1>
+            <img
+              class="pb-3"
+              src="../assets/img-login.png"
+              responsive
+              style="
+            max-width: 80%;
+            max-height: 80%;"
+            />
+          </v-col>
+
+          <v-col class="order-1 pa-3 mr-4" cols="12" md="1">
+            <v-card
+              class="pa-4"
+              color="grey lighten-3"
+              style="min-width: 300px;
+                     max-height: 300px;"
+            >
+              <v-layout column align-center>
+                <h2>Please Log In</h2>
+                <v-text-field label="Employee ID" outlined></v-text-field>
+                <v-text-field label="Password" outlined></v-text-field>
+                <v-btn class="login-btn" color="yellow" style="width: 50px">
+                  login
+                </v-btn>
+              </v-layout>
+            </v-card>
+          </v-col>
+        </v-card>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginView",
+  data: () => ({
+    alignments: ["start,end"]
+  })
+};
+</script>
+
+<style scoped>
+.some-height {
+  height: 100px;
+  width: 100%;
+}
+</style>
