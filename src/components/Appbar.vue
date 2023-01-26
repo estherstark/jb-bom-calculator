c2990b3985515ee0d392d9b47d36be02da330818
esther <phattara.nrrt@gmail.com>
2023-01-26 10:27:14 +0000

    Update 5 files

--- src/components/Appbar.vue
+++ src/components/Appbar.vue
@@ -38,42 +38,6 @@
       >
     </v-toolbar>
   </v-app-bar>
-
-  <!--<template>
-  <div>
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link v-if="!isLoggedIn" to="/login">Login</router-link>
-      <span v-if="isLoggedIn">{{username}}</span>
-      <router-link v-if="isLoggedIn" to="/logout">Logout</router-link>
-    </nav>
-    <router-view/>
-  </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      isLoggedIn: false,
-      username: ""
-    }
-  },
-  methods: {
-    login() {
-      // Code to handle login
-      this.isLoggedIn = true;
-      this.username = "John Doe";
-    },
-    logout() {
-      // Code to handle logout
-      this.isLoggedIn = false;
-      this.username = "";
-    }
-  }
-}
-</script> -->
 </template>
 
 <script>
@@ -98,3 +62,5 @@ export default {
   }
 };
 </script>
+
+<style></style>
--- src/components/InputCal.vue
+++ src/components/InputCal.vue
@@ -3,6 +3,43 @@
     <h1 class="ma-4 font-weight-regular">
       Input Details for Calculating
     </h1>
+
+    <v-card
+      class="pa-4 ma-2 d-flex justify-center align-center"
+      color="grey lighten-3"
+      style="min-width: 300px;"
+    >
+      <v-layout column align-center class="mt-3 mb-3" v-for="n in 3" :key="n">
+        <v-row>
+          <v-col>
+            <span>Set {{ n }}: </span>
+          </v-col>
+          <v-col>
+            <v-text-field
+              outlined
+              placeholder="Enter Job Name/ID"
+              type="text"
+              max-width="200px"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-spacer></v-spacer>
+        <v-row>
+          <v-col>
+            <span>Number of Set:</span>
+          </v-col>
+          <v-col>
+            <v-text-field
+              outlined
+              placeholder="Enter Number of Set"
+              type="text"
+              max-width="200px"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-layout>
+    </v-card>
+    <v-btn @click="nextView" depressed color="yellow">Next</v-btn>
   </v-app>
 </template>
 
